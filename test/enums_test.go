d6f54f8db3835174c6f522c82572a283ef02d4f8
Michał Kutrzeba <michal.kutrzeba@g2a.com>
2021-07-21 14:12:24 +0200

    test: added simple test for enums

---
+++ test/enums_test.go
@@ -0,0 +1,18 @@
+package test
+
+import (
+	"encoding/json"
+	enums "github.com/michalq/generate/test/enums_gen"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestEnums(t *testing.T) {
+	t.Run("it should unmarshal to struct with enum", func(t *testing.T) {
+		test := `{"currencyCode": "EUR"}`
+		payload := &enums.Root{}
+		err := json.Unmarshal([]byte(test), payload)
+		assert.Nil(t, err)
+		assert.Equal(t, payload.CurrencyCode, enums.EnumCurrencyCodeEUR)
+	})
+}
